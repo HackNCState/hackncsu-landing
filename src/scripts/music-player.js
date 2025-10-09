706efa86f07dd448ae263ef9a3e3bbfa9e1e3784
cgsizem3_ncstate <cgsizme3@ncsu.edu>
2025-10-09 14:52:25 -0400

    For #14, minor updates to fading and design of button

--- src/scripts/music-player.js
+++ src/scripts/music-player.js
@@ -3,18 +3,22 @@ function playMusic() {
     const cd = document.getElementById("cd-image");
 
     if (audio.paused) {
-        audio.volume = 0;
+        audio.volume = 0.03;
         audio.play();
         cd.classList.add("spinning");
+        light.style.backgroundColor = "limegreen";
+        light.style.boxShadow = "0 0 4px limegreen";
         const fadeIn = setInterval(() => {
             if (audio.volume < 1) {
-                audio.volume = Math.min(audio.volume + 0.025, 1);
+                audio.volume = Math.min(audio.volume + 0.0025, 1);
             } else {
                 clearInterval(fadeIn);
             }
         }, 100);
     } else {
         audio.pause();
+        light.style.backgroundColor = "red";
+        light.style.boxShadow = "0 0 4px red";
         cd.classList.remove("spinning");
     }
 }
